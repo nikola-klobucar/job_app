7263dea8b82274144c04878c43ec9cb9d1effb8c
nikola-klobucar <nikola.klobucar1@gmail.com>
2021-10-05 12:06:10 +0200

    Minor changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 //= require popper
+require("stylesheets/application.scss")
 import 'bootstrap'
 
 import Rails from "@rails/ujs"
